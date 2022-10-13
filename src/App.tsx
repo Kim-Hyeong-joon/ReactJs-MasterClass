49ffe19b12b63346dbd253401eb4420390159c0d
Hyeong-Joon <gudwns1020@naver.com>
2022-10-13 23:15:23 +0900

    #3.1 DefinitelyTyped

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Title>Hello</Title>
+    </Wrapper>
+  );
+}
+
+export default App;
