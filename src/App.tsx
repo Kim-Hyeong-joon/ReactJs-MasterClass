c20a0c9f60fb80ff71810d97341fc217d88db37e
Hyeong-Joon <gudwns1020@naver.com>
2022-10-20 22:42:10 +0900

    #4.7 Nested Routes part Two

--- src/App.tsx
+++ src/App.tsx
@@ -51,9 +51,9 @@ table {
 }
 body {
 	font-weight: 300;
-  font-family: 'Source Sans Pro', sans-serif;
-  background-color: ${(props) => props.theme.bgColor};
-  color:${(props) => props.theme.textColor};
+  	font-family: 'Source Sans Pro', sans-serif;
+  	background-color: ${(props) => props.theme.bgColor};
+  	color:${(props) => props.theme.textColor};
 	line-height: 1.2;
 }
 a{
