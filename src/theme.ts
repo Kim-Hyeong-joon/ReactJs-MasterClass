bfc16a3bbb4582ad1f8ac195e413fccd6b484f55
Hyeong-Joon <gudwns1020@naver.com>
2022-10-20 16:08:07 +0900

    #4.3-4.4 Home Screen

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
+  bgColor: "#2c3e50",
+  textColor: "#ecf0f1",
+  accentColor: "#4cd137",
 };
