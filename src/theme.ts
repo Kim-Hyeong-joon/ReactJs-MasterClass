a8a24d2a32b78b1828c2b0fbdf85956f6914d417
Hyeong-Joon <gudwns1020@naver.com>
2022-10-23 14:43:32 +0900

    #5.0 Dark Mode part one

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,15 @@
 import { DefaultTheme } from "styled-components";
 
-export const theme: DefaultTheme = {
+export const darkTheme: DefaultTheme = {
   bgColor: "#2c3e50",
-  textColor: "#ecf0f1",
+  textColor: "white",
   accentColor: "#4cd137",
+  cardBgColor: "transparent",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  textColor: "black",
+  accentColor: "#4cd137",
+  cardBgColor: "white",
 };
